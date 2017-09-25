dd0f43a928cbd0d091bb23fe86cdc3b5b7d8a296
Hugo Bravo Diaz <hbravod@gamma16.aulas.gsyc.urjc.es>
2017-09-25 16:36:50 +0200

    calc ejer5

--- calcoohija.py
+++ calcoohija.py
@@ -5,23 +5,22 @@
 import sys
 from calcoo import Calculadora
 
-class CalculadoraHija():
-
-    def __init__(self, operando3, operando4):
-        self.operando3 = operando3
-        self.operando4 = operando4
-
+class CalculadoraHija(Calculadora):
+   
     def multiplica(self):
-        return self.operando3 * self.operando4
+        return self.operando1 * self.operando2
     
     def divide(self):
-        return self.operando3 / self.operando4
+        try:
+            return self.operando1 / self.operando2
+        except ZeroDivisionError:
+            sys.exit("Division by zero is not allowed.")
 
     if len(sys.argv) != 4:
         sys.exit("Úsalo así: pyhton3 calcoohija.py operando1 operador operando2")
     try:
-        operando3 = int(sys.argv[1])
-        operando4 = int(sys.argv[3])
+        operando1 = int(sys.argv[1])
+        operando2 = int(sys.argv[3])
     except ValueError:
         sys.exit("Error: Non numerical parameters. Only 'int' parameters.")
 
@@ -29,13 +28,16 @@ if __name__ == "__main__":
 
     operacion = CalculadoraHija(int(sys.argv[1]), int(sys.argv[3]))
 
-    if sys.argv[2] == "multiplica":
+    if sys.argv[2] == "suma":
+        resultado = operacion.suma()
+
+    elif sys.argv[2] == "resta":
+        resultado = operacion.resta()
+
+    elif sys.argv[2] == "multiplica":
         resultado = operacion.multiplica()
 
     elif sys.argv[2] == "divide":
-        try:
-            resultado = operacion.divide()
-        except ZeroDivisionError:
-            sys.exit("Division by zero is not allowed.")
+        resultado = operacion.divide()
 
     print(resultado)
