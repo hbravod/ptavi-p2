a2dfa00992628a1fe95acc0ddfec35471f4b402c
Hugo Bravo Diaz <hbravod@gamma06.aulas.gsyc.urjc.es>
2017-10-02 15:56:21 +0200

    pep8 check

--- calcplus.py
+++ calcplus.py
@@ -2,32 +2,25 @@
 
 # -*- coding: utf-8 -*-
 
-#import csv
 import sys
 
 from calcoohija import CalculadoraHija
 
 if len(sys.argv) != 2:
     sys.exit("Úsalo así: python3 calcplus.py fichero")
-        
 
 if __name__ == "__main__":
 
-#    with open('calcplus.txt', newline= '') as csvfile:
-#        texto = csv.reader(csvfile, delimiter=' ', quotechar='|')
-#        for row in texto:
-#            print(', '.join(row))
+    fichero = open('/home/alumnos/hbravod/ptavi/ptavi-p2/operaciones', 'r')
+    num = fichero.readlines()
 
-    fichero=open('/home/alumnos/hbravod/ptavi/ptavi-p2/operaciones','r')
-    num=fichero.readlines()
-
-    
     for line in num:
-        lista=line.split(',')
-        primero=lista[0]
+        lista = line.split(',')
+        primero = lista[0]
 
         if primero == "suma":
-            resultado1 = int(lista[1])+int(lista[2])+int(lista[3])+int(lista[4])+int(lista[5])
+            resultado = int(lista[1])+int(lista[2])+int(lista[3])+int(lista[4])
+            resultado1 = resultado+int(lista[5])
             print(resultado1)
         elif primero == "resta":
             resultado2 = int(lista[1])-int(lista[2])
@@ -38,8 +31,8 @@ if __name__ == "__main__":
             print(resultado3)
         elif primero == "multiplica":
             resultado4 = int(lista[1])*int(lista[2])*int(lista[3])
-            print(resultado4)   
+            print(resultado4)
         elif primero == "divide":
             resultado5 = int(lista[1])/int(lista[2])/int(lista[3])
             print(resultado5)
-fichero.close      
+fichero.close
