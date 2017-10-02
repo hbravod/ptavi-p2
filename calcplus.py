3c4053e9958cc6ce0c6b50d049cb45fa5db50d8a
Hugo Bravo Diaz <hbravod@gamma06.aulas.gsyc.urjc.es>
2017-10-02 15:22:10 +0200

    falta resta

--- calcplus.py
+++ calcplus.py
@@ -5,7 +5,7 @@
 #import csv
 import sys
 
-#from calcoohija import CalculadoraHija
+from calcoohija import CalculadoraHija
 
 if len(sys.argv) != 2:
     sys.exit("Úsalo así: python3 calcplus.py fichero")
@@ -20,12 +20,41 @@ if __name__ == "__main__":
 
     fichero=open('/home/alumnos/hbravod/ptavi/ptavi-p2/operaciones','r')
     num=fichero.readlines()
-    
+
     
     for line in num:
-        lista=line.split()
-        #print(lista)
-        primero= line[:line.find(',')]
-        print(primero)
+        lista=line.split(',')
+        primero=lista[0]
 
-         
+        if primero == "suma":
+            resultado1 = int(lista[1])+int(lista[2])+int(lista[3])+int(lista[4])+int(lista[5])
+            print(resultado1)
+        elif primero == "resta":
+            resultado2 = int(lista[1])-int(lista[2])-int(lista[3])-int(lista[4])-int(lista[5])
+            print(resultado2)
+        elif primero == "multiplica":
+            resultado3 = int(lista[1])*int(lista[2])*int(lista[3])
+            print(resultado3)   
+        elif primero == "divide":
+            resultado4 = int(lista[1])/int(lista[2])/int(lista[3])
+            print(resultado4)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+fichero.close      
